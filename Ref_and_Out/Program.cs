4d568e51a797f752875fcb924632a3ca71f484d6
ermias-1 <vv8702in@go.minnstate.edu>
2020-02-13 03:02:43 -0600

    Adding comments on the code of the cs file, about the difference between ref and out keywords

--- Ref_and_Out/Program.cs
+++ Ref_and_Out/Program.cs
@@ -10,13 +10,21 @@ namespace Ref_and_Out
             string name = Console.ReadLine();
             string message = MakeGreeting(name);
             Console.WriteLine(message);
-
+            /* Both ref and out keywords are used to pass arguments into methods
+             * But out is used when the data is passed from the method. So, it is useful to return
+             * more than one value from the method
+             *While ref is used when the data is passed to the method
+             * Therefore, any change made to the parameter inside the method doesn't affect the original value */ 
             Console.WriteLine("Enter your last name:");
             string lastName = Console.ReadLine();
-            int nameLength = FullName(name, lastName, out string fullname);
+            /* out is used here because both the nameLength and fullname variables are
+             * initialized by the FullName() method */
+            int nameLength = FullName(name, lastName, out string fullname); 
             Console.WriteLine("Your full name is " + fullname + ". It has this many letters: " + nameLength);
 
-            Capitalize(ref fullname);
+            /* ref is used here the Capitalize() method call 
+             * and passes the original value to the method */
+            Capitalize(ref fullname); 
             Console.WriteLine("Your name in upper case: " + fullname);
 
             Console.WriteLine("Press any key to exit");
@@ -27,6 +35,7 @@ namespace Ref_and_Out
             return "Hello " + name + "!!";
         }
 
+        /* Both out and ref are used in the methods definition */
         static int FullName(string firstName, string lastName, out string fullName)
         {
             fullName = firstName + " " + lastName;
